f00b0706523a540da4c8dbb7a047fe8a392d766c
perla.stto <perla.stto@gmail.com>
2020-11-17 16:13:30 -0300

    db local pruebas

--- src/util/db.js
+++ src/util/db.js
@@ -1,22 +1,4 @@
-
-export default {
-	getById: async id =>{
-	/*DATA.then(db =>{
-
-	})*/
-	console.log('en la funcion fetb id')
-	console.log('el data es',DATA)
-
-    return await DATA.then(z=>{
-    	console.log('el z en',z)
-    	return z.find(x=>x.id==id)
-    })
-
-}
-}
-
-
-
+/*
 const DATA = (() => new Promise(done=>{
 	fetch('http://localhost:3100')
 	.then(r => r.json())
@@ -26,6 +8,39 @@ const DATA = (() => new Promise(done=>{
 
 	})
 }))();
+*/
+
+import d from './royal-battle-akes.json';
+const DATA = Promise.resolve(d)
+
+export default {
+	getById: id => DATA.then(z=>{
+		let item = z.find(x=>x.id==id) || {}
+		item.made_by = z.filter(x=>item.by.includes(x.id)) // review
+		item.mat_for = z.filter(x=>item.for.includes(x.id))
+	}),
+
+	getAll: () => DATA.then(all=>{
+	  let R={}
+	  all.forEach(x=>{
+		if(!R[x.type]) R[x.type] = {}
+	    if(!R[x.type][x.qlty]) R[x.type][x.qlty] = []
+	    R[x.type][x.qlty].push(x)
+      })
+      return R
+	}),
+	
+	getByQlty: (data,qlty) => data.then(z=> z.filter(x=>x.qlty==qlty)),
+
+	getByType: (data,type) => data.then(z=> z.filter(x=>x.type==type)),
+
+
+
+}
+
+
+
+
 /*
 const DATA = (() => Promise.resolve(new Promise(done=>{
 	fetch('http://localhost:3100')
