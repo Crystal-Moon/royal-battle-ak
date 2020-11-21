66a77f79c83ddd8e54a57153db9c6bfcf3a2dd5e
CrystalMoon <crystal.marino345@gmail.com>
2020-11-21 15:55:49 -0300

    funcional zoom basico

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Event } from './util/Event';
+
 //import logo from './logo.svg';
 import './assets/css/gral.css';
 import './assets/css/royal.css';
@@ -9,15 +11,34 @@ import Main from './components/Main';
 import Footer from './components/Footer'
 
 class App extends Component {
+  
+  constructor(){
+    super();
+    this.changeZoom = this.changeZoom.bind(this)
+    this.state = {
+      style: {
+        fontSize: '1rem'
+      }
+    }
+
+
+    Event.on('changeZoom', this.changeZoom)
+  }
+
 
   componentDidMount(){
   	
   	//require('./assets/css/themes/nowa.css'); //funciona genial :D
   }
+
+  changeZoom({ zoom }){
+    let z = zoom / 100
+    this.setState({ style:{ fontSize:`${z}rem`}})
+  }
   
   render() {
     return (
-      <div className="Todo">
+      <div className="Todo" style={ this.state.style } >
         <Header></Header>
         <Main></Main>
         <Footer></Footer>
--- src/AppMini.jsx
+++ src/AppMini.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Event } from './util/Event';
+
 //import logo from './logo.svg';
 import './assets/css/gral.css';
 import './assets/css/royal-mini.css'; // no llamar para app comun
@@ -8,15 +10,31 @@ import PrincipalMini from "./components/PrincipalMini";
 import LeftBar from "./components/LeftBar";
 
 class AppMini extends Component {
-	
+	constructor(){
+    super();
+    this.changeZoom = this.changeZoom.bind(this)
+    this.state = {
+      style: {
+        fontSize: '1rem'
+      }
+    }
+    Event.on('changeZoom', this.changeZoom)
+  }
+
+
   componentDidMount(){
-  	
-  	//require('./assets/css/themes/nowa.css'); //funciona genial :D
+    
+    //require('./assets/css/themes/nowa.css'); //funciona genial :D
+  }
+
+  changeZoom({ zoom }){
+    let z = zoom / 100
+    this.setState({ style:{ fontSize:`${z}rem`}})
   }
 
   render() {
     return (
-      <div className="Todo">
+      <div className="Todo" style={ this.state.style } >
         <PrincipalMini></PrincipalMini>
         <LeftBar></LeftBar>
       </div>
