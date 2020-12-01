c590decea4cd1fb309a547b5075fdce3f7b30d50
perla.stto <perla.stto@gmail.com>
2020-12-01 13:35:58 -0300

    diseño de tablas

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { render } from 'react-dom';
 import App from './App';
 //import AppMini from './AppMini'; // no importar en App
 //import 'react-app-polyfill/ie11';
 //import 'react-app-polyfill/stable';
 
-ReactDOM.render(
+render(
   <App />,
   document.getElementById('root')
 );
