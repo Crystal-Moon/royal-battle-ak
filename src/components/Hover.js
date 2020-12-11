98db82e4fb15d60a201b3d53e6332ad7e78b5b63
CrystalMoon <crystal.marino345@gmail.com>
2020-12-11 20:36:37 -0300

    ...

--- src/components/Hover.js
+++ src/components/Hover.js
@@ -3,31 +3,16 @@ import { Component } from 'react';
 import ItemIcon from './ItemIcon';
 
 class Hover extends Component {
-  constructor(){
-    super();
-  /*  this.state = {
-        noHover: false
-    }*/
-  }
-
-  componentDidMount(){
-    /* llamar configuracion de usuario y preguntar por el no-hover */
-   // this.setState({ noHover: true }) // cargar aqui lo del usuario
-  }
-
   render() {
-    //console.log('el prop q llega a hover sin const',this.props);
     const { item={} } = this.props;
     return( 
       <div className="Hover" >
-        {/* <div className={`Hover ${this.state.noHover?'no-hover':''}`} > */}
-    	<div className="item-head">
+    	  <div className="item-head">
             <span>
                 <ItemIcon item={ item } />
             </span>
-            <span className="item-name">{ item.name }</span>
+            <span className={`item-name ${item.qlty}`}>{ item.name }</span>
         </div>  
-
         <div className="item-body">
             <hr />
             <p className="stat">{ item.desc }</p>
